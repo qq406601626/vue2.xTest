0b8a73ea51193f932d10542f959664f30b946a9d
guoshuai <406601626@qq.com>
2021-08-26 17:06:02 +0800

    修改

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app"></div>
+</template>
+<script>
+
+export default {
+  data() {
+    return {}
+  },
+  methods:{
+
+  },
+  mounted() { }
+}
+
+</script>
+<style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
