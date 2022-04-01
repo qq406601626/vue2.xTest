a939f732641c0ac2aa7b06c623a32e00bafea737
guoshuai <406601626@qq.com>
2022-04-01 13:20:06 +0800

    更改

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,10 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [{
+    path: '/',
+    component: () => import('@/views/index')
+}]
 
 const router = new VueRouter({
     routes
---
+++ src/views/button.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="button">
+    <span>button</span>
+    <div v-if="loading" class="loading"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loading: false
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.button {
+  width: 100px;
+  height: 50px;
+  background: dodgerblue;
+  border-radius: 25px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #ffffff;
+  font-size: 14px;
+
+  .loading {
+    width: 16px;
+    height: 16px;
+    border-radius: 50%;
+    background: indianred;
+  }
+}
+</style>
