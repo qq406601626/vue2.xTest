9189ad47940b9143a0b717d36978573df9094f42
guoshuai <406601626@qq.com>
2022-05-18 22:17:16 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <!--<router-view></router-view>-->
+    <div>app page</div>
     <div ref="box"></div>
   </div>
 </template>
@@ -14,12 +15,25 @@ export default {
 
   },
   mounted() {
-    // console.log(this)
-    // console.log(h1)
-    const h1 = this.$createElement('h1','1111111111')
-    console.log('xxxxx')
-    const dom = this.__patch__(null,h1)
-    document.body.appendChild(dom)
+    const data = {
+      name:'zhangsan'
+    }
+    const v_object = {
+      data(){
+        return data
+      },
+      render(h) {
+        return h('div',`name:${this.name}`)
+      }
+    }
+    const v_node = this.$createElement(v_object)
+    // 1、如果传第一个参数，则先把虚拟dom数据转换成真实dom，然后把生成的真实dom自动挂载到传入的节点中。
+    // 2、如果不传第一个参数，则先把虚拟dom数据转换成真实dom，然后会生成一空dom节点，
+    // 然后把生成的真实dom插入到前面生成的空dom节点中。然后返回该空dom节点。
+    this.__patch__(this.$refs.box,v_node)
+    setTimeout(()=>{
+      data.name = 'lisi'
+    },5000)
   }
 }
 
