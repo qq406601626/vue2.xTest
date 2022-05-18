0e9cfb08f7b4dc4656e5fbed20d31fd3014c2548
guoshuai <guoshuai@nextmar.com>
2022-05-18 18:29:54 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <div ref="box"></div>
   </div>
 </template>
 <script>
@@ -12,7 +13,14 @@ export default {
   methods:{
 
   },
-  mounted() { }
+  mounted() {
+    // console.log(this)
+    // console.log(h1)
+    const h1 = this.$createElement('h1','1111111111')
+    console.log('xxxxx')
+    const dom = this.__patch__(null,h1)
+    document.body.appendChild(dom)
+  }
 }
 
 </script>
